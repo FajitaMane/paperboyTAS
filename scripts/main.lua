52ccd4c5c17395ab370305683147046e67766061
John French <johnfren@indiana.edu>
2015-11-11 23:02:18 -0500

    removed points heuristic dupe issue, improved move_list data structure

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,3 +1,5 @@
+--speedometer = require "speedometer_module"
+
 local frame = 1;
 local file = '../data/run1.fm2';
 
@@ -126,9 +128,6 @@ function is_in_menu()
 			end
 		end
 	end
-	if (frame % 100 == 0) then
-		emu.print("max pixels " .. max);
-	end
 	return max > 450;
 end
 
@@ -176,74 +175,7 @@ function tablelength(T)
   return count;
 end
 
---void. draws the white circle for the speedometer as well as the labels
-function drawspeedometer()
-	--these values are used to draw the speedometer
-	local cx = 235;
-	local cy = 210;
-	local radius = 15;
-	local speedometer_back = "white";
-	local speedometer_needle = "black";
-	local min_speed_angle = 100;
-	local max_speed_angle = 60;
-	local needle_radius_inner = 2;
-	local needle_radius_outer = 17;
-	local speedometer_unit_label = "mph";
 
-	--draw the speedometer background
-	for foo = 0, 360, 1 do
-		x = cx + radius * math.cos(foo);
-		y = cy + radius * math.sin(foo);
-		gui.setpixel(x, y, speedometer_back);
-		-- if this is the first pass, fill in the rows
-		if (foo < 180) then
-			left_bound = cx - (x - cx);
-			local cur = 0;
-			while (left_bound + cur < x) do
-				gui.setpixel(left_bound + cur, y, speedometer_back);
-				cur = cur + 1;
-			end
-		end
-	end
-
-	--calculate points for the needle based on current speed
-	--the first point is the one that points to the speed
-	speedf = (bike_speed() / 255);
-	if (speedf <= 0.30) then
-		speedf = 0;
-	else 
-		-- states speed as a float between 0 and 1
-		speedf = speedf - 0.30;
-		speedf = speedf * (1 / 0.7);
-	end
-	gui.text(10, 30, "Speed " .. speedf);
-	needle_angle = min_speed_angle + math.floor(speedf * ((360 - min_speed_angle) + max_speed_angle));
-	x1 = cx + needle_radius_outer * math.cos(math.rad(needle_angle));
-	y1 = cy + needle_radius_outer * math.sin(math.rad(needle_angle));
-	x2 = cx + needle_radius_inner * math.cos(math.rad((360 - min_speed_angle) + max_speed_angle));
-	y2 = cy + needle_radius_inner * math.sin(math.rad((360 - min_speed_angle) + max_speed_angle));
-
-	gui.line(x1, y1, x2, y2, speedometer_needle);
-
-	-- draw the numbers around the speedometer
-	local labels_inc = 2;
-	local labels_min = 2;
-	local labels_max = 12;
-	local inc_angle_degrees = math.floor(((340 - min_speed_angle) + max_speed_angle) 
-			/ ((labels_max - labels_min) / labels_inc));
-	for fiveinc = labels_min, labels_max, labels_inc do
-		local margin_right = 5;
-		local margin_bottom = 3;
-		local label_x = (cx - margin_right) + radius
-			* math.cos(math.rad((min_speed_angle + (((fiveinc / labels_inc) - 1) * inc_angle_degrees)) % 360));
-		local label_y = (cy - margin_bottom) + radius 
-			* math.sin(math.rad((min_speed_angle + (((fiveinc / labels_inc) - 1) * inc_angle_degrees)) % 360));
-		gui.drawtext(label_x, label_y, "" .. fiveinc, speedometer_back, speedometer_needle);
-	end
-
-	-- draw the mph or kmph label on the game
-	gui.drawtext(cx + needle_radius_inner + 3, cy + radius, speedometer_unit_label, speedometer_back, speedometer_needle);
-end
 
 -- this variable stores the last AI paper toss
 last_toss = 0;
@@ -264,8 +196,25 @@ starting_frame = nil;
 last_frame_menu_text = "no";
 local last_reset_frame = 0;
 
+local move_list = {};
+move_list.moves = {};
+move_list.new = function(arg_frame, arg_button_state)
+	local move = {};
+	move.frame = arg_frame;
+	move.button_state = arg_button_state;
+	move_list.moves[#move_list] = move;
+end
+
+--this variable stores the index of the next known move
+local move_list_cur = 0;
+
+--initialize on the last score
+last_frame_score = 0;
+
 while (true) do
 	cur_lives = lives();
+	cur_score = total_score();
+
 	--reset the game if the game ends
 	if (cur_lives < 1 and gameisrunning and not is_in_menu() and frame - last_reset_frame > 30) then
 		cur_lives = 5;
@@ -297,37 +246,43 @@ while (true) do
 	frame_toss_bool = frame_has_paper_toss(frame - 1);
 	if frame_has_paper_toss_bool then
 		gui.text(50, 40, "button press at frame " .. frame - 1);
-		frame_has_paper_toss = false;
-	else
-		-- gui.text(50, 40, "button press not detected");
+		frame_has_paper_toss = false; 
 	end
+
+	--check if a known successful move is known for the current frame
+	if (#move_list.moves > 1 and frame - last_reset_frame == move_list[move_list_cur].frame) then
+		joypad.write(1, move_list[move_list_cur].button_state);
+		move_list_cur = move_list_cur + 1;
+	end
+
+	--randomly toss papers
 	if (frame % (math.random(300) + 50) == 0 and menu_text == "no") then
 		joypad.write(1, a_press);
 		last_toss = frame;
 		last_toss_score = total_score();
 	else
-		if (total_score() > last_toss_score) then
-			if (score_increased_last_frame) then
-				local cur_score = total_score();
-				score_chunk = score_chunk + (cur_score - last_toss_score);
-				score_delta_chunks[tablelength(score_delta_chunks)] = score_chunk;
-				score_increased_last_frame = true;
-			else 
-				successful_toss_frames[tablelength(successful_toss_frames)] = last_toss;
-				successful_toss_count = successful_toss_count + 1;
-			end
+		--check for score increase
+		score_delimit = 2000; --ms in between score increases
+		if (cur_score > last_frame_score and frame - last_toss < 1500 and menu_text == "no") then
+			score_increased_last_frame = true;
+		end
+		if (score_increased_last_frame and cur_score <= last_frame_score) then
+			move_list.new(last_frame, a_press);
+			emu.print("successful toss frame found at " .. (last_toss - last_reset_frame));
+			score_increased_last_frame = false;
 		end
 	end
 	--only draw the speedometer if the paperboy is delivering
 	if (menu_text == "no") then
-		drawspeedometer();
+		--speedometer.draw();
 	end
 
-	gui.text(100, 10, tablelength(successful_toss_frames) .. " succesful AI tosses");
+	gui.text(100, 10, tablelength(move_list.moves) .. " succesful AI tosses");
 	--gui.text(100, 40, #successful_toss_frames .. " successful_toss_frames found");
-	FCEU.frameadvance();
 	last_frame_menu_text = menu_text;
+	last_frame_score = cur_score;
 	frame = frame + 1;
+	FCEU.frameadvance();
 end
 
 taseditor.registerauto(get_buttons);
---
+++ scripts/speedometer_module.lua
@@ -0,0 +1,71 @@
+module("speedometermodule", package.seall)
+
+--void. draws the white circle for the speedometer as well as the labels
+function draw()
+	--these values are used to draw the speedometer
+	local cx = 235;
+	local cy = 210;
+	local radius = 15;
+	local speedometer_back = "white";
+	local speedometer_needle = "black";
+	local min_speed_angle = 100;
+	local max_speed_angle = 60;
+	local needle_radius_inner = 2;
+	local needle_radius_outer = 17;
+	local speedometer_unit_label = "mph";
+
+	--draw the speedometer background
+	for foo = 0, 360, 1 do
+		x = cx + radius * math.cos(foo);
+		y = cy + radius * math.sin(foo);
+		gui.setpixel(x, y, speedometer_back);
+		-- if this is the first pass, fill in the rows
+		if (foo < 180) then
+			left_bound = cx - (x - cx);
+			local cur = 0;
+			while (left_bound + cur < x) do
+				gui.setpixel(left_bound + cur, y, speedometer_back);
+				cur = cur + 1;
+			end
+		end
+	end
+
+
+	--calculate points for the needle based on current speed
+	--the first point is the one that points to the speed
+	speedf = (bike_speed() / 255);
+	if (speedf <= 0.30) then
+		speedf = 0;
+	else 
+		-- states speed as a float between 0 and 1
+		speedf = speedf - 0.30;
+		speedf = speedf * (1 / 0.7);
+	end
+	gui.text(10, 30, "Speed " .. speedf);
+	needle_angle = min_speed_angle + math.floor(speedf * ((360 - min_speed_angle) + max_speed_angle));
+	x1 = cx + needle_radius_outer * math.cos(math.rad(needle_angle));
+	y1 = cy + needle_radius_outer * math.sin(math.rad(needle_angle));
+	x2 = cx + needle_radius_inner * math.cos(math.rad((360 - min_speed_angle) + max_speed_angle));
+	y2 = cy + needle_radius_inner * math.sin(math.rad((360 - min_speed_angle) + max_speed_angle));
+
+	gui.line(x1, y1, x2, y2, speedometer_needle);
+
+	-- draw the numbers around the speedometer
+	local labels_inc = 2;
+	local labels_min = 2;
+	local labels_max = 12;
+	local inc_angle_degrees = math.floor(((340 - min_speed_angle) + max_speed_angle) 
+			/ ((labels_max - labels_min) / labels_inc));
+	for fiveinc = labels_min, labels_max, labels_inc do
+		local margin_right = 5;
+		local margin_bottom = 3;
+		local label_x = (cx - margin_right) + radius
+			* math.cos(math.rad((min_speed_angle + (((fiveinc / labels_inc) - 1) * inc_angle_degrees)) % 360));
+		local label_y = (cy - margin_bottom) + radius 
+			* math.sin(math.rad((min_speed_angle + (((fiveinc / labels_inc) - 1) * inc_angle_degrees)) % 360));
+		gui.drawtext(label_x, label_y, "" .. fiveinc, speedometer_back, speedometer_needle);
+	end
+
+	-- draw the mph or kmph label on the game
+	gui.drawtext(cx + needle_radius_inner + 3, cy + radius, speedometer_unit_label, speedometer_back, speedometer_needle);
+end
